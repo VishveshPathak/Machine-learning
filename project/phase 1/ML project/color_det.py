dcbf0ee3f8c18493679e7c5a408fe8d253b6b99b
VishveshPathak <58458932+VishveshPathak@users.noreply.github.com>
2020-11-13 22:16:11 +0530

    Add files via upload

---
+++ project/phase 1/ML project/color_det.py
@@ -0,0 +1,43 @@
+#import all libraries
+import cv2
+import pandas as pd
+import numpy as np
+#to read image
+img = cv2.imread('pic1.jpg')
+#check if image has been loaded correctly 
+Feature_vector=np.array(img)
+index=["color","color_name","hex","R","G","B","index"]
+dataset=pd.read_csv('colors.csv',names=index,header=None)
+df=pd.DataFrame(dataset)
+print(df[0])
+clicked = False
+r = g = b = xpos = ypos = 0
+
+#Reading csv file with pandas and giving names to each column
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+#function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+#function to get x,y coordinates of mouse double click
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+       
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_function)
